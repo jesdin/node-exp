26274c66fa964fe02d9af42a4f1c6363a3cd1ac4
Jesdin Raphael <jesdinraphael@gmail.com>
2018-06-29 21:55:10 +0530

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+const sass = require('node-sass');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css': 'sass/style.scss'
+        }
+      },
+      options: {
+        implementation: sass,
+        sourceMap: true,
+        includePaths: [
+          './bower_components/css-calc-mixin'
+        ]
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss', 'views/**/*.jade'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+};
+
